c8b8ad1ef949cc827f57af6ab228de355be74559
Lucas Maia <lucassoaresmaia@hotmail.com>
2025-01-18 01:42:33 -0300

    update dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,6 +6,22 @@ import time
 import pandas as pd
 import numpy as np
 
+def extract_cpu(input_string): 
+    match = re.search(r"cpu\d+", input_string)  # Matches "cpu" followed by one or more digits
+    if match:
+        return match.group(0)
+    else:
+        return "Unknown"
+
+
+def order_cpu_columns(df):
+
+    cpu_cols = [col for col in df.columns if col.startswith('cpu')]
+    cpu_cols.sort(key=lambda x: int(re.search(r'\d+', x).group(0))) # Extract and convert to int for sorting
+
+    return df[cpu_cols]
+
+
 def connect_redis():
     """Conecta ao Redis usando variáveis de ambiente."""
     redis_host = os.getenv('REDIS_HOST', 'localhost')
@@ -25,7 +41,7 @@ def fetch_metrics(redis_client, key):
 
 def process_metrics(metrics):
     """Converte as métricas em um DataFrame para visualização."""
-    cpu_metrics = {key: value for key, value in metrics.items() if "cpu" in key}
+    cpu_metrics = {extract_cpu(key): value for key, value in metrics.items() if "cpu" in key}
     
     other_metrics = {key: value for key, value in metrics.items() if "cpu" not in key}
 
@@ -37,42 +53,51 @@ def process_metrics(metrics):
 def main():
     os.environ["STREAMLIT_SERVER_PORT"] = "52046"
 
-    st.title("Dashboard de Monitoramento")
-    st.markdown("Exibe métricas computadas pelo servidor.")
+    st.header("Dashboard de Monitoramento de Recursos")
+
+
+    st.subheader("Taxa da Atualização")
+    refresh_rate = st.slider(label="Taxa de atualização (segundos)", 
+                                   min_value=1,
+                                   max_value=30,
+                                   value=5,
+                                   step=1,
+                                   label_visibility="collapsed")
+
+    placeholder = st.empty()
 
     redis_client = connect_redis()
     redis_output_key = os.getenv("REDIS_OUTPUT_KEY", "metrics-output")
 
-    st.sidebar.markdown("### Opções")
-    refresh_rate = st.sidebar.slider("Taxa de atualização (segundos)", 1, 30, 5)
-
-    st.header("Métricas")
-    placeholder = st.empty()
 
     while True:
         metrics = fetch_metrics(redis_client, redis_output_key)
 
         with placeholder.container():
             if metrics:
-                cpu_df, other_df = process_metrics(metrics)
+                
 
                 col1, col2 = st.columns(2)
 
                 with col1:
-                    st.subheader("Utilização da CPU")
-                    st.line_chart(cpu_df.set_index("CPU")["Utilização (%)"])
-
+                        if "percent-memory-cache" in m:
+                            memory_metric = f"{m['percent-memory-cache']:.2f} %"
+                            st.subheader("Percentage of Memory Caching Content")
+                            st.metric(label="Uso de Memória Cache", value=memory_metric, border=True, label_visibility="collapsed")
+                    
                 with col2:
-                    st.subheader("Uso de CPU por núcleo")
-                    st.bar_chart(cpu_df.set_index("CPU")["Utilização (%)"])
+                    if "percent-network-egress" in m:
+                        network_metric = f"{m['percent-network-egress']:.2f} %"
+                        st.subheader("Percentage of Outgoing Traffic Bytes")
+                        st.metric(label="Uso de Rede", value=network_metric, border=True, label_visibility="collapsed")
 
-                col3, col4 = st.columns(2)
+                cpu_df, other_df = process_metrics(metrics)
+                cpu_df["CPU"] = cpu_df["CPU"].str.extract(r"(\d+)").astype(int)
+                cpu_df = cpu_df.sort_values("CPU")
 
-                with col3:
-                    if "percent-memory-cache" in metrics:
-                        memory_metric = {"percent-memory-cache": metrics["percent-memory-cache"]}
-                        st.subheader("Uso de Memória Cache (percent-memory-cache)")
-                        st.bar_chart(pd.DataFrame(memory_metric.items(), columns=["Métrica", "Valor"]).set_index("Métrica")["Valor"])
+                st.subheader("Utilização da CPU por Núcleo (média móvel)")
+
+                st.bar_chart(data=cpu_df, x="CPU", y="Utilização (%)", use_container_width=True, horizontal=True)
 
             else:
                 st.warning("Nenhuma métrica encontrada no Redis.")
